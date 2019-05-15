f278376963ee86ac9800dc3e1a1c3d0bf626b253
Chenyang Li <lcycug@live.com>
2019-05-15 23:08:09 +0800

    go pointer

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	lastName  string
+	firstName string
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateName(firstName string) {
+	(*pointerToPerson).firstName = firstName
+}
+
 func main() {
-	//cards := newDeck()
-	cards := loadFromLocal()
-	cards.print()
-	//cards.saveToLocal()
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Harrison",
+		contactInfo: contactInfo{
+			zipCode: 123456,
+			email:   "test@test.com",
+		},
+	}
+	alex.updateName("Alexander")
+	alex.print()
 }
