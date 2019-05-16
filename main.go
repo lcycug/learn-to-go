0bf0d4a50ceb67eed2d473b89dac254aa324e328
Chenyang Li <lcycug@live.com>
2019-05-16 22:04:00 +0800

    primitive and reference

--- main.go
+++ main.go
@@ -7,18 +7,37 @@ type contactInfo struct {
 	zipCode int
 }
 
+type skillSet struct {
+	name  []string
+	years []int
+}
+
 type person struct {
 	lastName  string
 	firstName string
 	contactInfo
+	skillSet
 }
 
 func (p person) print() {
 	fmt.Printf("%+v", p)
 }
 
-func (pointerToPerson *person) updateName(firstName string) {
-	(*pointerToPerson).firstName = firstName
+func (p *person) updateName(firstName string) {
+	(*p).firstName = firstName
+}
+
+func (p *person) addSkill(skill string, year int) {
+	(*p).skillSet.name = append(p.skillSet.name, skill)
+	(*p).skillSet.years = append(p.skillSet.years, year)
+}
+
+func (p person) changeSkillYear(skill string, year int) {
+	for i := 0; i < len(p.skillSet.name); i++ {
+		if p.skillSet.name[i] == skill {
+			p.skillSet.years[i] = year
+		}
+	}
 }
 
 func main() {
@@ -29,7 +48,17 @@ func main() {
 			zipCode: 123456,
 			email:   "test@test.com",
 		},
+		skillSet: skillSet{
+			name: []string{
+				"HTML", "JavaScript", "Java",
+			},
+			years: []int{
+				1, 2, 3,
+			},
+		},
 	}
 	alex.updateName("Alexander")
+	alex.addSkill("CSS", 2)
+	alex.changeSkillYear("Java", 10)
 	alex.print()
 }
